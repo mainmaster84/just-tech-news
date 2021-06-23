5e45ef6b6fe336409e5719382ec5aef44ae4b216
Ronald Main <rbmain84@yahoo.com>
2021-06-22 19:56:36 -0500

    completed

--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,24 +1,25 @@
-async function upvoteClickHandler(event) {
+async function editFormHandler(event) {
   event.preventDefault();
 
+  const title = document.querySelector('input[name="post-title"]').value.trim();
   const id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1
   ];
-  const response = await fetch('/api/posts/upvote', {
+  const response = await fetch(`/api/posts/${id}`, {
     method: 'PUT',
     body: JSON.stringify({
-      post_id: id
+      title
     }),
     headers: {
       'Content-Type': 'application/json'
     }
   });
-  
+
   if (response.ok) {
-    document.location.reload();
+    document.location.replace('/dashboard/');
   } else {
     alert(response.statusText);
   }
 }
 
-document.querySelector('.upvote-btn').addEventListener('click', upvoteClickHandler);
+document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
